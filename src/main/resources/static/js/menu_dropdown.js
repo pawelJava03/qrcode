db2dcd1dce48e7e5939ab83abf411f48fcfe2731
Paweł <pa.st.a@wp.pl>
2024-08-19 17:55:46 +0200

    adding some styles to dashboard, added logo

--- src/main/resources/static/js/menu_dropdown.js
+++ src/main/resources/static/js/menu_dropdown.js
@@ -1,4 +1,5 @@
 document.getElementById("menu-icon").onclick = function() {
+    this.classList.toggle("change");
     var dropdownMenu = document.getElementById("dropdown-menu");
     dropdownMenu.classList.toggle("show");
 };
